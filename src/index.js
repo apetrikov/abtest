cfbe94b930257bdb667198cf939aa55da987c52e
apetrikov <jnmort@gmail.com>
2023-02-16 16:50:13 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+import { trackPageview, trackEvent } from "./analytics-api.js";
+
+// Your code here
+
+// Parse HTML for test info
+// Parse cookies
+// Compare cookies and HTML
+// Apply final variant changes to HTML
+
+
+// TODO
+
+// Work with HTML service
+// Work with cookie service
+// Work with analytics service (analytics-api.js)
+// Log errors servise
