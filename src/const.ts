eee629d6760a5e6babd20a03e22accfd5ca065a6
apetrikov <jnmort@gmail.com>
2023-02-17 17:06:43 +0300

    fix: public path

--- src/const.ts
+++ src/const.ts
@@ -2,4 +2,4 @@ export const CTA_SELECTOR = 'data-cta' // for the button
 export const userId = '1' // for the test reason id is here as a const
 export const isRegisteredUser = false // We need only unregistered users
 
-export const publicURL = '/abtest/'
+export const publicURL = '/abtest'
