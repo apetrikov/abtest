610cccab88a2bea32135f87b41da5cf3a7d28942
apetrikov <jnmort@gmail.com>
2023-02-17 17:27:10 +0300

    Updates

--- src.f10117fe.js
+++ src.f10117fe.js
@@ -576,7 +576,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64286" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50726" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
